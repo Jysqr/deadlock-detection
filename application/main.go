8dd8ac41e34f33378ff1688ce959e47481ecd5a9
tylerjleary <leary@unbc,ca>
2020-02-22 13:44:39 -0800

    implementing network library

--- application/main.go
+++ application/main.go
@@ -4,14 +4,31 @@ import (
 	"deadlockdetection/node"
 	"fmt"
 	"github.com/perlin-network/noise"
+	"github.com/perlin-network/noise/kademlia"
+	"strconv"
+	"time"
+)
+
+var (
+	numNode       = 8
+	thisNode, err = noise.NewNode(noise.WithNodeBindPort(39999))
+	network       = kademlia.New()
 )
 
 func main() {
-	var run bool = true
-	//get int from gui
-
-	for run {
-		fmt.Println("wow")
+	thisNode.Bind(network.Protocol())
+	if err := thisNode.Listen(); err != nil {
+		panic(err)
 	}
-
+	address := thisNode.Addr()
+	//var run = true
+	//get int from gui
+	for i := 0; i < numNode; i++ {
+		fmt.Println(strconv.Itoa(i) + " init")
+		go func(i int) {
+			node.Start(i, address)
+		}(i)
+	}
+	time.Sleep(3 * time.Second)
+	fmt.Printf("Boss discovered %d peer(s).\n", len(network.Discover()))
 }
--- node/node.go
+++ node/node.go
@@ -1,21 +1,33 @@
 package node
 
 import (
+	"context"
 	"fmt"
 	"github.com/perlin-network/noise"
 	"github.com/perlin-network/noise/kademlia"
+	"strconv"
 )
 
 var (
-	nodeNum   int
-	network   = kademlia.New()
-	node, err = noise.NewNode()
+	nodeNum int
+	network        = kademlia.New()
+	port    uint16 = 40000
+	node    *noise.Node
+	err     error
 )
 
-func start(num int) {
+func Start(num int, bossAddr string) {
+	nodeNum = num
+	fmt.Println(strconv.Itoa(nodeNum) + " " + strconv.Itoa(int(port)+nodeNum))
+	node, err = noise.NewNode(noise.WithNodeBindPort(port + uint16(nodeNum)))
 	if err != nil {
 		panic(err)
 	}
 	node.Bind(network.Protocol())
-
+	if err := node.Listen(); err != nil {
+		panic(err)
+	}
+	if _, err := node.Ping(context.TODO(), bossAddr); err != nil {
+		panic(err)
+	}
 }
